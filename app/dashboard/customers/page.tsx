ef96752ee63e4e20b886c44e651917f0fc10dc3d
dev-ojeda <neo1sr3@gmail.com>
2024-10-14 15:29:53 -0300

    Update Nav Links

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,4 @@
+import { lusitana } from "@/app/ui/fonts";
+export default function Page() {
+  return <p className={`${lusitana.className} antialiased`}>Customers Page</p>;
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
+import React from 'react'
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
-}) {
+}): React.JSX.Element {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
